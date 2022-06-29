64b71e0d1cedd301c2c17c858e7dcdf9f28244dc
talles-alexandre <alexandretalles13@gmail.com>
2022-06-29 10:52:36 -0300

    criação de função criar quizz

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-function criarQuizz() {}
+function criarQuizz() {
+  const makequizz = document.querySelector(".make-quizz");
+  const allquizzes = document.querySelector(".all-quizzes");
+  makequizz.classList.add("escondido");
+  allquizzes.classList.add("escondido");
+}
