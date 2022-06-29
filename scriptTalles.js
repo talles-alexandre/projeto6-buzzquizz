2a8da69fda95db5f38ee7866ef99146834ba5357
talles-alexandre <alexandretalles13@gmail.com>
2022-06-29 17:19:20 -0300

    Layout tela comece pelo começo feito

--- scriptTalles.js
+++ scriptTalles.js
@@ -1,1 +1,8 @@
-//Falaaa Talles, aqui vc pode focar na tela 3 do projeto! É nóis!! *Pode apagar isso depois kkkkkkkkkk
+function criarQuizz() {
+  const containerVazio = document.querySelector(".containerVazio");
+  const todosQuizzes = document.querySelector(".todosQuizzes");
+  const crearTela0 = document.querySelector(".crearQuizz0");
+  containerVazio.classList.add("escondido");
+  todosQuizzes.classList.add("escondido");
+  crearTela0.classList.remove("escondido");
+}
