215ea813c44f18808fc8286f4dfd5d1d21eaf881
talles-alexandre <alexandretalles13@gmail.com>
2022-06-30 10:03:20 -0300

    Criação de layout estático Crie suas perguntas

--- scriptTalles.js
+++ scriptTalles.js
@@ -42,5 +42,29 @@ function validarInformacoesBasica() {
 }
 
 function renderizarPerguntas() {
-  alert("Tudo Ok");
+  const crearTela0 = document.querySelector(".crearQuizz0");
+  crearTela0.classList.add("escondido");
+  const perguntas = document.querySelector(".perguntas");
+  perguntas.classList.remove("escondido");
+
+  const caixaperguntas = document.querySelector(".caixa-perguntas");
+
+  caixaperguntas.innerHTML = `
+  <div class="pergunta">
+          <p>Pergunta 1</p>
+          <input type="text" class="pergunta1" placeholder="Texto da pergunta">
+          <input type="text" class="pergunta1-cor" placeholder="Cor de fundo da pergunta">
+          <p>Resposta correta</p>
+          <input type="text" class="resposta-correta" placeholder="Resposta correta">
+          <input type="text" class="url-img" placeholder="URL da imagem">
+          <p>Resposta incorretas</p>
+          <input type="text" class="resposta-incorreta1" placeholder="Resposta incorreta 1">
+          <input type="text" class="url-img1" placeholder="URL da imagem 1">
+          <input type="text" class="resposta-incorreta2" placeholder="Resposta incorreta 2">
+          <input type="text" class="url-img2" placeholder="URL da imagem 2">
+          <input type="text" class="resposta-incorreta3" placeholder="Resposta incorreta 3">
+          <input type="text" class="url-img3" placeholder="URL da imagem 3">
+        </div>
+  
+  `;
 }
