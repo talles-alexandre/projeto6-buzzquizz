22a30c76c0b853eb768c0c00a155f5650e8c0e57
talles-alexandre <alexandretalles13@gmail.com>
2022-07-01 11:13:10 -0300

    função renderizar perguntas

--- scriptTalles.js
+++ scriptTalles.js
@@ -1,7 +1,11 @@
-let tituloQuizz = "";
-let urlImagemPrincipal = "";
-let quantidadePerguntas = 0;
-let quantidadeNiveis = 0;
+let quizzCriado = {
+  title: "",
+  image: "",
+  quantidadePerguntas: 0,
+  quantidadeNiveis: 0,
+  questions: [],
+  levels: [],
+};
 
 function criarQuizz() {
   const containerVazio = document.querySelector(".containerVazio");
@@ -17,6 +21,11 @@ function criarPerguntas() {
   urlImagemPrincipal = document.querySelector(".imagemDoQuizz").value;
   quantidadePerguntas = document.querySelector(".qtdPerguntas").value;
   quantidadeNiveis = document.querySelector(".qtdNiveis").value;
+
+  quizzCriado.title = tituloQuizz;
+  quizzCriado.image = urlImagemPrincipal;
+  quizzCriado.quantidadePerguntas = parseInt(quantidadePerguntas);
+  quizzCriado.quantidadeNiveis = parseInt(quantidadeNiveis);
   validarInformacoesBasica();
 }
 
@@ -28,13 +37,13 @@ function validarInformacoesBasica() {
     quantidadeNiveis === ""
   ) {
     alert("Campo vazio, preencher os dados corretamente.");
-  } else if (tituloQuizz.length < 20 || tituloQuizz.length > 65) {
+  } else if (quizzCriado.title.length < 20 || quizzCriado.title.length > 65) {
     alert("Título inválido");
-  } else if (!urlImagemPrincipal.includes("https:")) {
+  } else if (!quizzCriado.image.includes("https:")) {
     alert("Url inválida");
-  } else if (quantidadePerguntas < 3) {
+  } else if (quizzCriado.quantidadePerguntas < 3) {
     alert("Quantidade de perguntas inválida");
-  } else if (quantidadeNiveis < 2) {
+  } else if (quizzCriado.quantidadeNiveis < 2) {
     alert("Quantidade de níveis inválido");
   } else {
     renderizarPerguntas();
@@ -49,22 +58,47 @@ function renderizarPerguntas() {
 
   const caixaperguntas = document.querySelector(".caixa-perguntas");
 
-  caixaperguntas.innerHTML = `
-  <div class="pergunta">
-          <p>Pergunta 1</p>
-          <input type="text" class="pergunta1" placeholder="Texto da pergunta">
-          <input type="text" class="pergunta1-cor" placeholder="Cor de fundo da pergunta">
-          <p>Resposta correta</p>
-          <input type="text" class="resposta-correta" placeholder="Resposta correta">
-          <input type="text" class="url-img" placeholder="URL da imagem">
-          <p>Resposta incorretas</p>
-          <input type="text" class="resposta-incorreta1" placeholder="Resposta incorreta 1">
-          <input type="text" class="url-img1" placeholder="URL da imagem 1">
-          <input type="text" class="resposta-incorreta2" placeholder="Resposta incorreta 2">
-          <input type="text" class="url-img2" placeholder="URL da imagem 2">
-          <input type="text" class="resposta-incorreta3" placeholder="Resposta incorreta 3">
-          <input type="text" class="url-img3" placeholder="URL da imagem 3">
-        </div>
-  
-  `;
+  for (let i = 0; i < quizzCriado.quantidadePerguntas; i++) {
+    caixaperguntas.innerHTML += `
+       
+              <div class="pergunta">
+                <p>Pergunta ${i + 1}</p>
+                <input type="text" class="pergunta${
+                  i + 1
+                }" placeholder="Texto da pergunta">
+                <input type="text" class="pergunta${
+                  i + 1
+                }-cor" placeholder="Cor de fundo da pergunta">
+
+                <p>Resposta correta</p>
+
+                <input type="text" class="pergunta-${
+                  i + 1
+                }-resposta-correta" placeholder="Resposta correta">
+                <input type="text" class="pergunta-${
+                  i + 1
+                }-url-correta" placeholder="URL da imagem">
+
+                <p>Resposta incorretas</p>
+
+                <input type="text" class="resposta-pergunta-${
+                  i + 1
+                }-incorreta-1" placeholder="Resposta incorreta 1">
+                <input type="text" class="url" placeholder="URL da imagem 1">
+
+
+                <input type="text" class="resposta-pergunta-${
+                  i + 1
+                }-incorreta-2" placeholder="Resposta incorreta 2">
+                <input type="text" class="url" placeholder="URL da imagem 2">
+
+
+                <input type="text" class="resposta-pergunta-${
+                  i + 1
+                }-incorreta-3" placeholder="Resposta incorreta 3">
+                <input type="text" class="url-img3" placeholder="URL da imagem 3">
+              </div>
+        
+        `;
+  }
 }
